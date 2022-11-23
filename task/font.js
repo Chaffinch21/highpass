1e5b842223fd4062dfea0a2164855f6e5ab03e44
Chaffinch21 <lena_431@mail.ru>
2022-11-23 09:05:07 +0700

    удалила ttf2woff2

--- task/font.js
+++ task/font.js
@@ -1,24 +1,23 @@
 const {src, dest} = require('gulp');
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
+// const plumber = require('gulp-plumber');
+// const notify = require('gulp-notify');
 const path = require('../config/path');
-const app = require('../config/app');
+// const app = require('../config/app');
 const newer = require('gulp-newer');
-const fonter = require('gulp-fonter');
-const ttf2woff2 = require('gulp-ttf2woff2');
+// const fonter = require('gulp-fonter');
 
 const font = () => {
   return src(path.font.src)
-  .pipe(plumber({
-    errorHandler: notify.onError(error => ({
-      title: "Font",
-      message: error.message
-    }))
-  }))
-  .pipe(newer(path.font.dest))
-  .pipe(fonter(app.fonter))
-  .pipe(dest(path.font.dest))
-  .pipe(ttf2woff2())
+  // .pipe(plumber({
+  //   errorHandler: notify.onError(error => ({
+  //     title: "Font",
+  //     message: error.message
+  //   }))
+  // }))
+  // .pipe(newer(path.font.dest))
+  // .pipe(fonter(app.fonter))
+  // .pipe(dest(path.font.dest))
+  // .pipe(ttf2woff2())
   .pipe(dest(path.font.dest))
 }
 
--- task/resources.js
+++ task/resources.js
@@ -3,7 +3,6 @@ const {src, dest} = require('gulp');
 const path = require("../config/path.js");
 
 const resources = () => {
-  console.log(path.resources.src)
   return src(path.resources.src)
   .pipe(dest(path.resources.dest));
 }
